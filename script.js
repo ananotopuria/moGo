9e97ddd556d52954f86e07f98fb985bc3b328801
Anano Topuria <anano.topuriaa@gmail.com>
2022-12-25 21:23:48 +0400

    sticky navigation

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+`use strict`;
+
 // Accordion section
 const toggles = document.querySelectorAll(`.acc-toggle`);
 toggles.forEach((toggle) => {
@@ -5,3 +7,28 @@ toggles.forEach((toggle) => {
     toggle.parentNode.classList.toggle(`active`);
   });
 });
+
+// Sticky Navigation
+
+const sectionHeroEl = document.querySelector(`.section-hero`);
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+    if (ent.isIntersecting === false) {
+      document.body.classList.add(`sticky`);
+    }
+    if (ent.isIntersecting) {
+      document.body.classList.remove(`sticky`);
+    }
+  },
+
+  {
+    // in the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: `-80px`,
+  }
+);
+
+obs.observe(sectionHeroEl);
