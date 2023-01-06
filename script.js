9a4213eafd27d66768b5238a3e2fbc307bd0f423
Anano Topuria <anano.topuriaa@gmail.com>
2023-01-06 13:06:58 +0400

    slider

--- script.js
+++ script.js
@@ -30,7 +30,6 @@ const obs = new IntersectionObserver(
     rootMargin: `-80px`,
   }
 );
-
 obs.observe(sectionHeroEl);
 
 // Smooth scrolling
@@ -56,3 +55,61 @@ btnScrollTo.addEventListener("click", function (e) {
   const s1coords = section1.getBoundingClientRect();
   section1.scrollIntoView({ behavior: `smooth` });
 });
+
+// slider
+const slider = function () {
+  const slides = document.querySelectorAll(`.slide`);
+
+  const sliderr = document.querySelector(`.slider`);
+  sliderr.style.transform = `scale(0.2)`;
+  sliderr.style.overflow = `visible`;
+
+  const btnLeft = document.querySelector(`.slider__btn--left`);
+  const btnRight = document.querySelector(`.slider__btn--right`);
+
+  let curSlide = 0;
+  const maxSlide = slides.length;
+
+  // Functions
+
+  const goToSlide = function (slide) {
+    slides.forEach(
+      (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
+    );
+  };
+
+  // Next slide
+
+  const nextSlide = function () {
+    if (curSlide === maxSlide - 1) {
+      curSlide = 0;
+    } else {
+      curSlide++;
+    }
+    goToSlide(curSlide);
+  };
+
+  const prevSlide = function () {
+    if (curSlide === 0) {
+      curSlide = maxSlide - 1;
+    } else {
+      curSlide--;
+    }
+    goToSlide(curSlide);
+  };
+  const init = function () {
+    goToSlide(0);
+  };
+
+  init();
+
+  // Event handlers
+  btnRight.addEventListener(`click`, nextSlide);
+  btnLeft.addEventListener(`click`, prevSlide);
+
+  document.addEventListener(`keydown`, function (e) {
+    if (e.key === `ArrowLeft`) prevSlide();
+    e.key === `ArrowRight` && nextSlide();
+  });
+};
+slider();
